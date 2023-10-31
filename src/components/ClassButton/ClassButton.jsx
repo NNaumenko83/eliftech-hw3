7d3a0f779ca763b74231be896550e787e65e54ed
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-10-31 22:35:55 +0200

    CREATE project

---
+++ src/components/ClassButton/ClassButton.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ClassButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <button type="button"></button>;
+  }
+}
+
+export default ClassButton;
---
+++ src/components/FunctionalButton/FunctionalButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FunctionalButton = () => {
+  return <div></div>;
+};
+
+export default FunctionalButton;
