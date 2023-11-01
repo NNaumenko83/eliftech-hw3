43726be49e01d1ae02c96aa40fce3b718ef006a8
Nikolay Naumenko <nnv2006@ukr.net>
2023-11-01 15:59:18 +0200

    ADD: breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.scss";
 import ClassButton from "./components/ClassButton/ClassButton";
+import Container from "./components/Container/Container";
 import FunctionalButton from "./components/FunctionalButton/FunctionalButton";
 
 function App() {
-  return (
-    <div className="App">
-      <ClassButton />
-      <FunctionalButton />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Container>
+				<ClassButton />
+				<FunctionalButton />
+			</Container>
+		</div>
+	);
 }
 
 export default App;
