06faaa44a07d96c1d3cc89e9d4bfe442c149b71f
Nikolay Naumenko <nnv2006@ukr.net>
2023-11-01 13:40:16 +0200

    ADD: mixins

--- src/components/FunctionalButton/FunctionalButton.jsx
+++ src/components/FunctionalButton/FunctionalButton.jsx
@@ -3,18 +3,12 @@ import { ButtonText } from "../ButtonText/ButtonText";
 import styles from "./_FunctionalButton.module.scss";
 
 const FunctionalButton = () => {
-  const [counter, setCounter] = useState(0);
-  return (
-    <div>
-      <button
-        className={styles.functionalButton}
-        type="button"
-        onClick={() => setCounter(counter + 1)}
-      >
-        FunctionalButton click: <ButtonText value={counter} />
-      </button>
-    </div>
-  );
+	const [counter, setCounter] = useState(0);
+	return (
+		<button className={styles.functionalButton} type="button" onClick={() => setCounter(counter + 1)}>
+			FunctionalButton click: <ButtonText value={counter} />
+		</button>
+	);
 };
 
 export default FunctionalButton;
