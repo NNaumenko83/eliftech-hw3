f2656800c4663b4ad1cfe778b0507e125298dd8c
Nikolay Naumenko <nnv2006@ukr.net>
2023-11-01 16:08:31 +0200

    ADD: link to repo

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,17 @@ function App() {
 				<ClassButton />
 				<FunctionalButton />
 			</Container>
+			<h3>
+				Link to repo:
+				<a
+					target="blank"
+					rel="noopener nofollow noreferrer"
+					style={{ textAlign: "center", color: "blue" }}
+					href="https://github.com/NNaumenko83/eliftech-hw3"
+				>
+					GitHub
+				</a>
+			</h3>
 		</div>
 	);
 }
